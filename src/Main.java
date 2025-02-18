0cbaabb1ce24a8c776e2966d38e0874ba6c7ae25
astonishiing <wcardoso4510@gmail.com>
2025-02-18 14:13:08 -0300

    implementado objetos na classe Main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+         //Animal n = new Animal();
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+
+        m.setPeso(35.3f);
+        m.setCorPelo("Marrom");
+        m.alimentar();
+        m.locomover();
+        m.emitirSom();
+
+        p.locomover();
+        r.locomover();
+
     }
 }
