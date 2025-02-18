9ff87687c1a592ea93bf465b09259f83b53e36a4
astonishiing <wcardoso4510@gmail.com>
2025-02-18 14:08:40 -0300

    implementação do Override e criação de classes

--- src/Ave.java
+++ src/Ave.java
@@ -3,16 +3,27 @@ public class Ave extends Animal {
 
     @Override
     public void locomover() {
-
+        System.out.println("Voando");
     }
 
     @Override
     public void alimentar() {
-
+        System.out.println("Comendo Frutas");
     }
 
     @Override
     public void emitirSom() {
+        System.out.println("Som de ave");
+    }
+    public void fazendoNinho(){
+        System.out.println("Construindo ninho");
+    }
 
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+
+    public String getCorPena() {
+        return corPena;
     }
 }
