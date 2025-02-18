2a033486c88e61cda81e5128e22de97bd5138751
astonishiing <wcardoso4510@gmail.com>
2025-02-18 14:06:40 -0300

    criação da classe ave

---
+++ src/Ave.java
@@ -0,0 +1,18 @@
+public class Ave extends Animal {
+    private String corPena;
+
+    @Override
+    public void locomover() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+
+    @Override
+    public void emitirSom() {
+
+    }
+}
