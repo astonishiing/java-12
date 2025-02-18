8660efb976155b92d6408245f0961772cdd29d10
astonishiing <wcardoso4510@gmail.com>
2025-02-18 13:46:03 -0300

    criação de classe-abstrata

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,10 @@
-public class Animal {
+public abstract class Animal {
     protected float peso;
     protected int idade;
     protected int membros;
+
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirsom();
+
 }
