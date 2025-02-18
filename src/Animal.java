e81f996faabb76a962877c8c8ea301a107928222
astonishiing <wcardoso4510@gmail.com>
2025-02-18 13:47:35 -0300

    comentários

--- src/Animal.java
+++ src/Animal.java
@@ -1,8 +1,10 @@
 public abstract class Animal {
+    //Atributos
     protected float peso;
     protected int idade;
     protected int membros;
 
+    //Métodos abstratos - somente as filhas podem ser desenvolvidas
     public abstract void locomover();
     public abstract void alimentar();
     public abstract void emitirsom();
